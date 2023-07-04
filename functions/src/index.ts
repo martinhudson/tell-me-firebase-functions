549bd083b1364189caf238895f791a9ff53affe6
martinhudson <martin.hudson@me.com>
2023-07-04 12:58:42 +0100

    fixed createUserDocument

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import * as logger from "firebase-functions/logger";
+// import * as logger from "firebase-functions/logger";
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 
@@ -9,25 +9,26 @@ const db = admin.firestore();
 /**
  * Create user in Firestore on user account creation through Firebase Auth.
  */
-export const createUserDocument = () => {
-  //adding a comment
-  functions.auth.user().onCreate(async (user) => {
-    const newUser = {
-      uid: user.uid,
-      email: user.email,
-      displayName: user.displayName,
-      providerData: user.providerData,
-    };
-    logger.info("new user created", user.email, user.uid);
-    db.collection("users").doc(user.uid).set(newUser);
-  });
-};
+// eslint-disable-next-line max-len
+export const createUserDocument = functions.auth.user().onCreate(async (user) => {
+  /**
+   * Cherry pick user data to only send
+   * what we actually need to the client.
+   */
+  const newUser = {
+    uid: user.uid,
+    email: user.email,
+    displayName: user.displayName,
+    providerData: user.providerData,
+  };
+
+  db.collection("users").doc(user.uid).set(newUser);
+});
 
 /**
  * Delete user in Firestore on user account deletion through Firebase Auth.
  */
-export const deleteUserDocument = () => {
-  functions.auth.user().onDelete(async (user) => {
-    db.collection("users").doc(user.uid).delete();
-  });
-};
+// eslint-disable-next-line max-len
+export const deleteUserDocument = functions.auth.user().onDelete(async (user) => {
+  db.collection("users").doc(user.uid).delete();
+});
